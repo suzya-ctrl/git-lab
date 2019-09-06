55977ea2e9a6cb3d648700525b5c8d24996b5d17
Suzan Abdelqader <sa434816@ohio.edu>
2019-09-06 17:44:23 -0400

    git -lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Suzan Abdelqader>
+ *        Date: <09/06/19>
+ * Description: C++ program made for lab 2 in relation to git hub
+ */
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
